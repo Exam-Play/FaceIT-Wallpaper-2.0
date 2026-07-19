362f3183b9ba21088795ab6f523d48a6427c6f50
Andrew <97516045+Exam-Play@users.noreply.github.com>
2026-07-20 02:56:56 +0300

    ADD/BACKEND: A new endpoint for the last five matches has been added

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,16 @@
 import httpx
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import cloudscraper
 
 from config import FACEIT_API_KEY
+from functions import map_match
 
 HEADERS = { "Authorization": f"Bearer {FACEIT_API_KEY}" }
 
 app = FastAPI()
+
+scraper = cloudscraper.create_scraper()
 
 app.add_middleware(
     CORSMiddleware,
@@ -75,3 +79,26 @@ async def get_main_elo(nickname: str):
         "region_rank": region_rank["position"],
         "country_rank": country_rank["position"]
     }
+
+@app.get("/recent-matches")
+async def get_last_matches(nickname: str):
+    async with httpx.AsyncClient() as client:
+        player_response = await client.get(
+            "https://open.faceit.com/data/v4/players",
+            params={"nickname": nickname},
+            headers=HEADERS
+        )
+        player_response.raise_for_status()
+        player_data = player_response.json()
+        player_id = player_data["player_id"]
+        level = player_data["games"].get("cs2", {}).get("skill_level")
+
+        rounds_response = scraper.get(
+            f"https://www.faceit.com/api/statistics/v1/cs2/players/{player_id}/match-rounds",
+            params={"limit": 5}
+        )
+
+        rounds_response.raise_for_status()
+        rounds = rounds_response.json()["payload"]["cs2"]["match_rounds"]
+
+        return [map_match(r, level) for r in rounds[:5]]
